393b567b9c31621539d7208129cb95a09ba2e062
Oumatou <oumatoulacen@gmail.com>
2024-06-26 17:12:47 +0100

    with Jinja templates

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,7 +1,8 @@
+from flask import Flask, render_template
 import feedparser
-from flask import Flask
 
-RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+RSS_FEEDS = {
+    'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
     'cnn': 'http://rss.cnn.com/rss/edition.rss',
     'fox': 'http://feeds.foxnews.com/foxnews/latest',
     'iol': 'http://www.iol.co.za/cmlink/1.640'}
@@ -10,23 +11,21 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<publication>")
-def bbc(publication="bbc"):
-    if publication not in RSS_FEEDS:
-        publication = "bbc"
-    return get_news(publication)
+def bbc(publication="bbc"):    
+    feeds = feedparser.parse(RSS_FEEDS[publication])
+    # print(feeds)
+    return render_template("home.html", articles=feeds['entries'])
 
-def get_news(publication):
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-    <body>
-    <h1>Headlines </h1>
-    <b>{0}</b> </ br>
-    <i>{1}</i> </ br>
-    <p>{2}</p> </ br>
-    </body>
-    </html>""".format(first_article.get("title"), first_article.
-    get("published"), first_article.get("summary"))
+
+@app.route("/print_ansi")
+def print_ansi():
+    # ANSI escape codes for red color and reset
+    RED = '\033[91m'
+    RESET = '\033[0m'
+
+    # Your print statement with ANSI color codes
+    print(f'{RED}cnn news:{RESET} {RSS_FEEDS["bbc"]}')
+    return f'{RED}cnn news:{RESET} {RSS_FEEDS["bbc"]}'
 
 
 if __name__ == "__main__":
